88ab065bdb6d15114d6f4dd3b76446984ae24e2d
mertkozcan <mertkozcan@outlook.com>
2016-12-09 15:02:54 +0300

    test

--- app/src/main/java/com/bmyazilim/soruyorum/MainActivity.java
+++ app/src/main/java/com/bmyazilim/soruyorum/MainActivity.java
@@ -2,6 +2,14 @@ package com.bmyazilim.soruyorum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://soruyorum.bmyazilim.net/index.php?username=mertat&name=asastt&surname=at&password=at&mail=at&pictureid=1";
+
+        StringRequest request=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Toast.makeText(MainActivity.this, "Cevap : "+response, Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Hata : "+error.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        queue.add(request);
     }
 }
