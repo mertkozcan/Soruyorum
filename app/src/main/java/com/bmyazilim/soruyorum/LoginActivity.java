301f81db491164bd6c12578b8edfb70718f3ad45
mertkozcan <mertkozcan@outlook.com>
2016-12-13 15:27:49 +0300

    login

---
+++ app/src/main/java/com/bmyazilim/soruyorum/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.bmyazilim.soruyorum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.facebook.FacebookSdk;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "DlpF2zsN4tmcsOhIcHSzojAiV";
+    private static final String TWITTER_SECRET = "oiGE1ZwbE88uRPuOj8iVuk51d2qxQksjMDNfrrDvtPNTFJLHXX";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+        FacebookSdk.sdkInitialize(getApplicationContext());
+//        AppEventsLogger.activateApp(this);
+        setContentView(R.layout.activity_login);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://soruyorum.bmyazilim.net/index.php?username=mertat&name=asastt&surname=at&password=at&mail=at&pictureid=1";
+
+        StringRequest request=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Toast.makeText(LoginActivity.this, "Cevap : "+response, Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this, "Hata : "+error.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+   //     queue.add(request);
+    }
+}
