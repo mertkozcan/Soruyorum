c582827e74575a2f61e06e9849992ce607e8eb50
mertkozcan <mertkozcan@outlook.com>
2017-02-02 13:12:22 +0300

    face twitter logins

--- app/src/main/java/com/bmyazilim/soruyorum/LoginActivity.java
+++ app/src/main/java/com/bmyazilim/soruyorum/LoginActivity.java
@@ -1,15 +1,40 @@
 package com.bmyazilim.soruyorum;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.bmyazilim.soruyorum.models.Users;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.SignInButton;
 import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterAuthClient;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -20,6 +45,9 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TWITTER_KEY = "DlpF2zsN4tmcsOhIcHSzojAiV";
     private static final String TWITTER_SECRET = "oiGE1ZwbE88uRPuOj8iVuk51d2qxQksjMDNfrrDvtPNTFJLHXX";
     VolleyStuff volleyStuff;
+    CallbackManager callbackManager;
+    TwitterLoginButton btntwit;
+    TwitterAuthClient authClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +55,96 @@ public class LoginActivity extends AppCompatActivity {
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
         FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
 //        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_login);
         volleyStuff=new VolleyStuff();
 
+        final EditText txtmail=(EditText)findViewById(R.id.input_email);
+        final EditText txtsifre=(EditText)findViewById(R.id.input_password);
+        Button btngiris=(Button)findViewById(R.id.btn_login);
+        LoginButton btnface=(LoginButton)findViewById(R.id.btn_face);
+         btntwit=(TwitterLoginButton)findViewById(R.id.btn_twit);
+        SignInButton btngoogle=(SignInButton)findViewById(R.id.btn_google);
 
+        btnface.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends"));
 
+        btngiris.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                volleyStuff.loginControl(txtmail.getText().toString(), txtsifre.getText().toString(), new VolleyStuff.VolleyCallback() {
+                    @Override
+                    public void onSuccess(String result) {
+                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+        btnface.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.v("LoginActivity", response.toString());
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        btntwit.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+
+                Toast.makeText(getApplicationContext(),result.data.getUserName(),Toast.LENGTH_LONG).show();
+
+            authClient.requestEmail(result.data, new Callback<String>() {
+                @Override
+                public void success(Result<String> result) {
+
+                    Toast.makeText(getApplicationContext(),result.data,Toast.LENGTH_LONG).show();
+
+                }
+
+                @Override
+                public void failure(TwitterException exception) {
+
+                }
+            });
+
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        btntwit.onActivityResult(requestCode, resultCode, data);
     }
 }
 
--- app/src/main/java/com/bmyazilim/soruyorum/VolleyStuff.java
+++ app/src/main/java/com/bmyazilim/soruyorum/VolleyStuff.java
@@ -7,14 +7,48 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.google.gson.JsonArray;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by mertk on 12.12.2016.
  */
 public class VolleyStuff  {
 
+    public void loginControl(String mail,String password,final VolleyCallback callback){
+
+        String tag = "giriskontrol";
+
+        String url = "http://soruyorum.bmyazilim.net/kontrol.php?islem=giriskontrol&mail="+mail+"&password="+password;
+
+
+        StringRequest strReq = new StringRequest(Request.Method.GET,
+                url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                callback.onSuccess(response);
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+// Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag);
+
+    }
+
+    public interface VolleyCallback{
+        void onSuccess(String result);
+    }
 
 }
